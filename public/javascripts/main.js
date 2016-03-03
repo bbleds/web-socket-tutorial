2c6535296e01837eac15e038b7aef776b76bfa4a
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-03 11:34:30 -0600

    appending messages over the dom

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,28 @@
 ;(function () {
-  'use strict'
+  'use strict';
 
-  const ws = io.connect()
+  const ws = io.connect();
 
   ws.on('connect', () => {
-    console.log('socket connected')
-  })
+    console.log('socket connected');
+  });
+
+  const form = document.querySelector("form");
+  const name = document.querySelector("input[name='name']");
+  const text = document.querySelector("input[name='text']");
+  const ul = document.querySelector("ul");
+
+// when form is submitted, create li element and append to ul
+  form.addEventListener("submit", () => {
+    // prevent default submission
+    event.preventDefault();
+    // create li
+    let elemNode = document.createElement("h1");
+    // create text for li
+    let textNode = document.createTextNode(`${name.value}:  ${text.value}`);
+    // append created text to li
+    elemNode.appendChild(textNode);
+    // append li to ul
+    ul.appendChild(elemNode);
+  });
 }());
