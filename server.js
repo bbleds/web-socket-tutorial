427912e40fd11c6c61f2142f0fa4f46e0488de86
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-03 09:56:31 -0600

    initialized express

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 "use strict";
 // -------------- Dependencies
 //--- modules
+// express
+const express = require("express");
+const app = express();
 // Init naitive node server
-const server = require("http").createServer();
+const server = require("http").createServer(app);
 // pass server into socketio
 const io = require("socket.io")(server);
-
+// listen on process port or 3000
 const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Connected");
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
